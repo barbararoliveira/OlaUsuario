cf10081099bdbe8fde26e79f45398c30311cac2d
Bárbara <babyr.oliveira@gmail.com>
2020-03-13 22:45:22 -0300

    Resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Olá, " + nome + "!");
+            
+
+
+        }
+    }
+}
